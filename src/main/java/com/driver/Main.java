efd0d284214889ac648f5f7a8fb7759ff55ffe63
prajwalh02 <99700885+prajwalh02@users.noreply.github.com>
2023-08-09 13:51:41 +0530

    Attempt 1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,31 @@
 package com.driver;
 
+import java.sql.SQLOutput;
+
 public class Main {
 
+    public static class Product {
+        public int product(int x, int y) {
+            return x * y;
+        }
+        public int product(int x, int y, int z) {
+            return x * y * z;
+        }
+        public double product(double x, double y) {
+            return x * y;
+        }
+    }
+
+    public static void main(String[] args) {
+        Product p = new Product();
+
+        int result1 = p.product(5,3);
+        System.out.println("Product of 5 and 3: " + result1);
+
+        int result2 = p.product(2,3,4);
+        System.out.println("Product of 2, 4, and 3: " + result2);
+
+        double result3 = p.product(2.5, 3.0);
+        System.out.println("Product of 2.5 and 3.0: " + result3);
+    }
 }
